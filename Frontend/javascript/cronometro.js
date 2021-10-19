93a88dc37426242ec7df05e1ec10c3ead0a38085
Carlos Cardozo <carlos.jimenezc@cun.edu.co>
2021-10-19 14:48:14 -0500

    ya funciona u.u

--- Frontend/javascript/cronometro.js
+++ Frontend/javascript/cronometro.js
@@ -2,20 +2,51 @@ var minutoInicio = document.getElementById("minutos").innerHTML;
 var segundoInicio = document.getElementById("segundos").innerHTML;
 var minutooriginal=document.getElementById("minutos").innerHTML;
 var segundooriginal=document.getElementById("segundos").innerHTML;
+minDescanso=1;
+segDescanso=30;
 
+function actualizar(contador,bandera) {
 
-function actualizar(contador) {
+    if(bandera==1){
+        document.getElementById("minutos").innerHTML = minDescanso;
+        document.getElementById("segundos").innerHTML = segDescanso;
+        
+        if(segDescanso==0){
+            if(minDescanso==0 && segDescanso==0){
+                minDescanso=1;
+                segDescanso=30;
+                setTimeout(()=>{
+                    document.getElementById("numSerie").innerHTML =contador;
+                },1000)
+                var time = setTimeout(actualizar,1000,contador)
+            }else{
+                minDescanso-=1;
+                segDescanso=59;
+                var time = setTimeout(actualizar,1000,contador,1)
+            }
+        }else{
+            document.getElementById("numSerie").innerHTML ="Descanso";
+            segDescanso-=1;
+            var time = setTimeout(actualizar,1000,contador,1)
+        }
+    }else{
+
     document.getElementById("minutos").innerHTML = minutoInicio;
     document.getElementById("segundos").innerHTML = segundoInicio;
 
     if (segundoInicio == 0) {
         if (minutoInicio == 0 && segundoInicio == 0) {
-            if(contador<2){
+            if(contador<document.getElementById("series").innerHTML){
               minutoInicio=minutooriginal
               segundoInicio=segundooriginal
-             var time= setTimeout(actualizar, 1000,contador+1);
+                setTimeout(()=>{
+                    document.getElementById("numSerie").innerHTML =contador+1;
+                },1000)
+             var time= setTimeout(actualizar, 1000,contador+1,1);
             }
           else{
+            document.getElementById("serref").classList.add("no_mostrar");
+            document.getElementById("numSerie").innerHTML ="Finalizado";
               clearTimeout(time)
           }
         } else {
@@ -30,15 +61,8 @@ function actualizar(contador) {
         segundoInicio -= 1;
         var time= setTimeout(actualizar, 1000,contador);
     }
-
+   }
 }
 var cont=1;
 actualizar(cont)
-// for(var contador=1;contador<=series;contador++){
-//     if(contador==1){
-//         actualizar();
-//     }else{
-//         descanso()
-//         actualizar()
-//     }
-//
+
